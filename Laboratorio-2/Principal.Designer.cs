344a25176026beaeda9354dcc7ed03f4d084890f
Javier Forchiney <kingdario@gmail.com>
2025-09-13 15:14:16 -0500

    Botones en pantalla principal y en el codigo para que se muestre la pantalla principal.

--- Laboratorio-2/Principal.Designer.cs
+++ Laboratorio-2/Principal.Designer.cs
@@ -28,19 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.pnl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblPrincipal
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(291, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Pagina Principal";
+            this.lblPrincipal.AutoSize = true;
+            this.lblPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrincipal.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPrincipal.Location = new System.Drawing.Point(316, 39);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(161, 24);
+            this.lblPrincipal.TabIndex = 0;
+            this.lblPrincipal.Text = "Página Principal";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl3.Location = new System.Drawing.Point(456, 20);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(116, 24);
+            this.lbl3.TabIndex = 18;
+            this.lbl3.Text = "Problema 3";
+            this.lbl3.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl2.Location = new System.Drawing.Point(241, 20);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(116, 24);
+            this.lbl2.TabIndex = 17;
+            this.lbl2.Text = "Problema 2";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl1.Location = new System.Drawing.Point(53, 20);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(116, 24);
+            this.lbl1.TabIndex = 16;
+            this.lbl1.Text = "Problema 1";
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(258, 171);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(82, 41);
+            this.btn5.TabIndex = 15;
+            this.btn5.Text = "Ir";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(471, 171);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(82, 41);
+            this.btn6.TabIndex = 14;
+            this.btn6.Text = "Ir";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(471, 53);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(82, 41);
+            this.btn3.TabIndex = 13;
+            this.btn3.Text = "Ir";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(258, 53);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(82, 41);
+            this.btn2.TabIndex = 12;
+            this.btn2.Text = "Ir";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(67, 171);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(82, 41);
+            this.btn4.TabIndex = 11;
+            this.btn4.Text = "Ir";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(67, 53);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(82, 41);
+            this.btn1.TabIndex = 10;
+            this.btn1.Text = "Ir";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl4.Location = new System.Drawing.Point(53, 133);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(116, 24);
+            this.lbl4.TabIndex = 19;
+            this.lbl4.Text = "Problema 4";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl5.Location = new System.Drawing.Point(241, 133);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(116, 24);
+            this.lbl5.TabIndex = 20;
+            this.lbl5.Text = "Problema 5";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl6.Location = new System.Drawing.Point(456, 133);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(116, 24);
+            this.lbl6.TabIndex = 21;
+            this.lbl6.Text = "Problema 6";
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.Maroon;
+            this.pnl1.Controls.Add(this.lbl6);
+            this.pnl1.Controls.Add(this.lbl4);
+            this.pnl1.Controls.Add(this.lbl5);
+            this.pnl1.Controls.Add(this.lbl1);
+            this.pnl1.Controls.Add(this.lbl3);
+            this.pnl1.Controls.Add(this.btn4);
+            this.pnl1.Controls.Add(this.lbl2);
+            this.pnl1.Controls.Add(this.btn1);
+            this.pnl1.Controls.Add(this.btn5);
+            this.pnl1.Controls.Add(this.btn6);
+            this.pnl1.Controls.Add(this.btn3);
+            this.pnl1.Controls.Add(this.btn2);
+            this.pnl1.Location = new System.Drawing.Point(107, 116);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(609, 271);
+            this.pnl1.TabIndex = 22;
             // 
             // Principal
             // 
@@ -48,9 +215,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkRed;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnl1);
+            this.Controls.Add(this.lblPrincipal);
             this.Name = "Principal";
             this.Text = "Principal";
+            this.pnl1.ResumeLayout(false);
+            this.pnl1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +228,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Panel pnl1;
     }
 }
--- Laboratorio-2/Principal.cs
+++ Laboratorio-2/Principal.cs
@@ -16,5 +16,46 @@ namespace Laboratorio_2
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Problema1 p1 = new Problema1();
+            p1.Show();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Problema2 p2 = new Problema2();
+            p2.Show();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Problema3 p3 = new Problema3();
+            p3.Show();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Problema4 p4 = new Problema4();
+            p4.Show();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Problema5 p5 = new Problema5();
+            p5.Show();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Problema6 p6 = new Problema6();
+            p6.Show();
+        }
     }
 }
